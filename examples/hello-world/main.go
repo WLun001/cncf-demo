7d7c66f17ad876cb819f75bae1d97796f9609a12
Wei Lun <weilun_95@hotmail.com>
2020-12-11 23:26:35 +0800

    add hello-world app

---
+++ examples/hello-world/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	host, _ := os.Hostname()
+	_, _ = fmt.Fprintf(w, "Hello, world!\n")
+	_, _ = fmt.Fprintf(w, "Version: 1.0.0\n")
+	_, _ = fmt.Fprintf(w, "Hostname: %s\n", host)
+}
