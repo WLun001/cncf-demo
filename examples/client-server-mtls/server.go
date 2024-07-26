b36ac6fd714ff89fcd9757629383e0d47bd28721
weilun <23547929+WLun001@users.noreply.github.com>
2024-07-26 17:43:36 +0800

    auto reload tls for server

--- examples/client-server-mtls/server.go
+++ examples/client-server-mtls/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"go-mtls/utils"
 	"log"
 	"net/http"
@@ -18,7 +19,6 @@ func main() {
 			return
 		}
 	})
-
 	caCert, err := os.ReadFile(utils.EnvDefault("CA_CERTS_PATH", "certs/ca.crt"))
 	if err != nil {
 		log.Fatal(err)
@@ -26,18 +26,11 @@ func main() {
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
 
-	// Load server's certificate and key
-	serverCert, err := tls.LoadX509KeyPair(
-		utils.EnvDefault("CERT_PATH", "certs/server.crt"), utils.EnvDefault("KEY_PATH", "certs/server.key"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Configure TLS with mTLS requirements
 	config := &tls.Config{
-		ClientCAs:    caCertPool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		Certificates: []tls.Certificate{serverCert},
+		ClientCAs:      caCertPool,
+		ClientAuth:     tls.RequireAndVerifyClientCert,
+		GetCertificate: GetCertificate,
 	}
 	server := &http.Server{
 		Addr:      utils.EnvDefault("SERVER_ADDR", ":8443"),
@@ -46,4 +39,20 @@ func main() {
 
 	log.Printf("Listening on %s\n", server.Addr)
 	log.Fatal(server.ListenAndServeTLS("", ""))
+}
+
+func GetCertificate(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	cert, err := tls.LoadX509KeyPair(
+		utils.EnvDefault("CERT_PATH", "certs/server.crt"),
+		utils.EnvDefault("KEY_PATH", "certs/server.key"),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not load TLS cert: %s", err)
+	}
+	parsedCert, err := x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		return nil, fmt.Errorf("could not parsed TLS cert: %s", err)
+	}
+	log.Printf("Loaded certificate: NotBefore: %s, NotAfter: %s", parsedCert.NotBefore, parsedCert.NotAfter)
+	return &cert, nil
 }
